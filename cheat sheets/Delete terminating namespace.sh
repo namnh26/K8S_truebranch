98bebf527b624ac71e2a4ed9379af3c20612dad9
mark.nguyen <mark.nguyen@pal.net.vn>
2022-08-06 22:21:39 +0700

    Update cheat sheets/Delete terminating namespace.sh

---
+++ cheat sheets/Delete terminating namespace.sh
@@ -0,0 +1,4 @@
+NAMESPACE=<Stuck namespace>
+kubectl get namespace $NAMESPACE -o json \
+  | tr -d "\n" | sed "s/\"finalizers\": \[[^]]\+\]/\"finalizers\": []/" \
+  | kubectl replace --raw /api/v1/namespaces/$NAMESPACE/finalize -f -
