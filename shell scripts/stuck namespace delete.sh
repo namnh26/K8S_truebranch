0715365cee23d86380fb6243b16f9881a2fa6016
Phương Nguyễn <thanhphuong23593@gmail.com>
2022-12-03 19:41:03 +0700

    Update shell script folder

---
+++ shell scripts/stuck namespace delete.sh
@@ -0,0 +1,4 @@
+NAMESPACE=stucknamespace
+kubectl get namespace $NAMESPACE -o json \
+  | tr -d "\n" | sed "s/\"finalizers\": \[[^]]\+\]/\"finalizers\": []/" \
+  | kubectl replace --raw /api/v1/namespaces/$NAMESPACE/finalize -f -
